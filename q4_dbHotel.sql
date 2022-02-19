d292f49820d7b7c3541b2e1eafcb4a833bd2620e
git init <butkepavan55@gmail.com>
2022-02-19 14:19:53 +0530

    q12 done db

---
+++ q4_dbHotel.sql
@@ -0,0 +1,10 @@
+tables(type, id, name, description, menu, address)
+hotel(name , address , description , id )
+menus (menu_id, name, price ) 
+hotels_menus (hotel_id, menu_id)
+
+--Retrieve order
+SELECT m.name,
+ m.price FROM hotel_menus AS hm INNER JOIN menus AS m
+ ON hm.hotel_id = [hotel_id]
+ AND hm.menu_id = m.menu_id
